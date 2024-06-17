36ac73efcc5e842cf25461efe0673f116ba9b98f
Santiago Aguirre <santiago.aguirre@tngtech.com>
2024-06-17 19:41:27 +0200

    Remove onClick handler

--- frontend/src/Components/CopyToClipboardButton/CopyToClipboardButton.tsx
+++ frontend/src/Components/CopyToClipboardButton/CopyToClipboardButton.tsx
@@ -2,10 +2,7 @@ import sharedClasses from '../../styles.module.css';
 import { BUTTON_COPY_TO_CLIPBOARD } from '../../constants';
 
 export const CopyToClipboardButton = () => (
-  <button
-    class={sharedClasses.button}
-    onClick={() => navigator.clipboard.writeText(`${location.href}`)}
-  >
+  <button class={sharedClasses.button} onClick={}>
     {BUTTON_COPY_TO_CLIPBOARD}
   </button>
 );
